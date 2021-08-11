ebd6f03ca0c3909ba521e97bb6b4748f410fea10
Felipe Barcellos <felipebarcellos11@gmail.com>
2021-08-11 12:23:09 -0300

    add nextauth initial

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,24 +2,32 @@ import styles from './styles.module.scss'
 
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, useSession, signOut } from 'next-auth/client'
+import { userInfo } from 'os'
+
+//useSession = se está ativo ou nao
 
 export function SingInButton() {
-    const isUserLoggedIn = true;
+    const [session] = useSession()
 
-    return isUserLoggedIn ? (
+    console.log(session)
+
+    return session ? (
         <button 
             type="button"
             className={styles.singInButton}
+            onClick={()=>signOut()}
         >
             
                 <FaGithub color="#04d361"/> 
-                Felipe Barcellos
+                {session.user.name}
                 <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ) : (
         <button 
             type="button"
             className={styles.singInButton}
+            onClick={()=>signIn('github')}
         >
             
                 <FaGithub color="#eba417"/> 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 import { AppProps } from 'next/app'
 import { Header } from '../components/Header'
+import { Provider as NexAuthProvider } from 'next-auth/client'
 
 import '../styles/global.scss'
 
 //fica por volta de tudo(repete em todas paginas) e é recarregado sempre que mudar de pagina
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NexAuthProvider session={pageProps.session}>
     <Header/>  
     <Component {...pageProps} />
-    </>
+    </NexAuthProvider>
   )
 }
 
